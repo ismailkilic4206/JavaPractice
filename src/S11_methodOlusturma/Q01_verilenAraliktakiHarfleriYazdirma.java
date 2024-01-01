1a0bf9383fd20a3b4af2fe2b952046810f7fbd2f
İsmail Kılıç <kaunistr@gmail.com>
2024-01-01 16:50:51 +0300

    24.01.01

---
+++ src/S11_methodOlusturma/Q01_verilenAraliktakiHarfleriYazdirma.java
@@ -0,0 +1,21 @@
+package S11_methodOlusturma;
+
+public class Q01_verilenAraliktakiHarfleriYazdirma {
+    public static void main(String[] args) {
+        //Soru 1-verilen bir String ifadeyi ters ceviren bir method olusturun
+        String kelime = "cumle";
+
+        System.out.println(verilenAraliktakiHarfleriYazdirma(kelime));
+        System.out.println(verilenAraliktakiHarfleriYazdirma("äli gel"));
+    }
+
+    public static String verilenAraliktakiHarfleriYazdirma(String kelime) {
+        String bosKova = "";
+        for (int i = kelime.length() - 1; i >= 0 ; i--) {
+            bosKova += kelime.charAt(i);
+        }
+        String tersMetin = bosKova;
+
+        return tersMetin;
+    }
+}
